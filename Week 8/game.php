d5ea921cd88c5623feb16dcaf23a250ec897dbc4
Aksharasuresh6818 <90092584+Aksharasuresh6818@users.noreply.github.com>
2022-05-19 10:57:31 +0530

    Update game.php

--- Week 8/game.php
+++ Week 8/game.php
@@ -45,7 +45,7 @@ $result = check($computer, $human);
 <!DOCTYPE html>
 <html>
 <head>
-<title>Anamitra Musib 32ce3337</title>
+<title>Akshara Suresh 1979ffdc</title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
